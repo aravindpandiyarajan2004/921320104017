b9e72104fa787673b355d53f6f00725ed98632ed
aravindpandiyarajan2004 <aravindpandiyarajan2004@psnacet.edu.in>
2023-08-14 23:15:36 -0700

    m

--- afford-test/src/App.js
+++ afford-test/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+//import AllTrainsPage from './components/AllTrainsPage.js';
+//import SingleTrainPage from './components/SingleTrainPage';
+
 
 function App() {
+  fetch("http://20.244.56.144/train/register")
+.then((res) => res.json()) 
+.then((data) =>{
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={AllTrainsPage} />
+        <Route path="/train/:trainNumber" component={SingleTrainPage} />
+      </Switch>
+    </Router>
   );
 }
 
